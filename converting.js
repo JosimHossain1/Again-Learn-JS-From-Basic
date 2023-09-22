96d4d1133be511b197cbb93eacf02fa954ecae69
Josim Hossain <josimhosssain842@gmail.com>
2023-09-22 13:31:25 +0600

    Learn Converting and solve some problems

---
+++ converting.js
@@ -0,0 +1,17 @@
+//01. Solving Shot problem
+
+const money = 1000;
+const cost = 700;
+
+const availableBalance = money - cost;
+console.log(availableBalance);
+
+// 02. Solving problem
+
+const math = 20.5;
+const bangla = 10;
+const english = 50;
+
+const total = parseFloat(math + bangla + english);
+const avarage = total / 2;
+console.log(avarage);
